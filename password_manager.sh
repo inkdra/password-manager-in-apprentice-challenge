a0da85ae656e4b761372e695b1565b8153c78ba5
inkdra <157680622+inkdra@users.noreply.github.com>
2024-02-16 16:13:19 +0900

    パスワード保存処理、保存先ファイルの追加

--- password_manager.sh
+++ password_manager.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
   
+echo "パスワードマネージャーへようこそ！"
+
+read -p "サービス名を入力してください:" serviceName
+read -p "ユーザー名を入力してください:" userName
+read -p "パスワードを入力してください:" password
+
+echo "Thank you!"
+
+echo " $serviceName : $userName : $password " >> passwordLibrary.txt   
  
