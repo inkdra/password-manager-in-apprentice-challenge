be5b7f35e50e9ebadf536149e8fc4c80a73ae45a
inkdra <157680622+inkdra@users.noreply.github.com>
2024-02-21 07:30:37 +0900

    条件式のクオーテーションマーク削除

--- password_manager.sh
+++ password_manager.sh
@@ -2,12 +2,10 @@
   
 echo "パスワードマネージャーへようこそ！"
 
-read -p "次の選択肢から入力してください(Add Password/Get Password/Exit)：" option
-
-while [ "$option" != "Exit" ]; 
+while [ $option != "Exit" ]; 
 do
 read -p "次の選択肢から入力してください(Add Password/Get Password/Exit)：" option  
-if [ "$option" = "Add Password" ]; then 
+if [ $option = "Add Password" ]; then 
    read -p "サービス名を入力してください:" serviceName
    read -p "ユーザー名を入力してください:" userName
    read -p "パスワードを入力してください:" password
@@ -15,14 +13,14 @@ if [ "$option" = "Add Password" ]; then
    echo " $serviceName : $userName : $password " >> passwordLibrary.txt
    echo "パスワードの追加は成功しました。"  
 
-elif [ "$option" = "Get Password" ]; then
+elif [ $option = "Get Password" ]; then
    read -p "サービス名を入力してください:" serviceName
    if grep -q $serviceName passwordLibrary.txt; then
       echo grep $serviceName passwordLibrary.txt
    else
       echo "そのサービスは登録されていません。"
    fi
-elif [ "$option" = "Exit" ]; then
+elif [ $option = "Exit" ]; then
    echo "Thank you!"
    break  
 fi
